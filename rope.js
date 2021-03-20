c3fd5df66f4f7bf2d384145d384ecd26ddbdc851
calvinH567 <76797277+calvinH567@users.noreply.github.com>
2021-03-19 19:27:31 -0700

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,17 @@
+class Rope {
+    constructor(bodyA,pointB){
+        var options = {
+            length: 100,
+            stiffness: 0.001,
+            bodyA: bodyA,
+            pointB: pointB
+        }
+        this.body = Constraint.create(options);
+        World.add(world,this.body)
+    }
+    display(){
+        strokeWeight(10);
+        line(this.body.bodyA.position.x,this.body.bodyA.position.y,
+            this.pointB.x,this.pointB.y)
+    }
+}
